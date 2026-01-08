fe2233f5da3263ba0c69ed32b1f85df195d952f7
adithya.balaji <adithya.balaji@neura-robotics.com>
2026-01-08 14:32:46 +0100

    update two_sum

--- 1_two_sum/1_two_sum.py
+++ 1_two_sum/1_two_sum.py
@@ -1,15 +1,13 @@
-class Solution(object):
+class Solution:
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-            
-        previous_map = {}
-        for ind,num in enumerate(nums):
-            diff = target-num
-            if diff in previous_map:
-                return [previous_map[diff], ind]
-            previous_map[num] = ind
-        return [] #if no solution is found
+        #create a hashmap to store the difference between the target and the current number
+        #the key is the difference, the value is the index of the current number
+        #if the number is not in the hashmap, add it to the hashmap
+        #if the number is in the hashmap, return the index of the current number and the index of the number in the hashmap
+        
+        hashmap  = {}
+        for ind, num in enumerate(nums):
+            if num not in hashmap:
+                hashmap[target-num]=ind
+            else:
+                return [hashmap[num], ind]
